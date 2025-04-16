5c92961c18331b218d626b265260d7cb4c81dcba
Субботин Дмитрий Алексеевич <subbotinus@gmail.com>
2025-04-16 16:17:14 +0500

    added login tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,7 +8,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.current_url, "Url without 'login'"
+        assert "login" in self.url, "Url without 'login'"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
