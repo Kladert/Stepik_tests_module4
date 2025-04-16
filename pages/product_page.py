6526637851c09bc74affb4ab2196a306ca840efd
Субботин Дмитрий Алексеевич <subbotinus@gmail.com>
2025-04-16 18:02:00 +0500

    iprov: data indepedence

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,8 +7,15 @@ class ProductPage(BasePage):
         add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_button.click()
         self.solve_quiz_and_get_code()
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        self.check_name_and_price_added_product(self.get_product_name(),self.get_product_price())
+
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        
+    def check_name_and_price_added_product(self,name,price):
         ls = [a.text for a in self.browser.find_elements(*ProductPageLocators.STRONG_TEXT_IN_MESSAGES)]
-        assert product_name in ls, "Wrong product added to basket"
-        assert product_price in ls, "Product`s price in basket not equal price on product page" 
+        assert name in ls, "Wrong product added to basket"
+        assert price in ls, "Product`s price in basket not equal price on product page"        
