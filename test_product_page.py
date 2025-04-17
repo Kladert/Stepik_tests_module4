eea1ef2e2be2a34078dfb61dec8ffabd1a589c75
Kladert <kladert@inbox.ru>
2025-04-17 11:20:55 +0500

    last corrections

--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,7 @@ from pages.login_page import LoginPage
 import time
 import pytest
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -11,6 +12,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     
+@pytest.mark.need_review    
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
@@ -19,6 +21,15 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_product_in_basket()
     basket_page.exists_message_basket_is_empty()
+    
+@pytest.mark.need_review   
+def test_guest_can_go_to_login_page_from_product_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page() 
     
 @pytest.mark.login_user 
 class TestUserAddToBasketFromProductPage():
@@ -30,12 +41,19 @@ class TestUserAddToBasketFromProductPage():
         email = str(time.time())+ "@fakemail.org"
         page.register_new_user(email,"zxc123vbn")
         page.should_be_authorized_user()
-    
+        
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self,browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)
         page.open()
-        time.sleep(5)
         page.add_product_to_basket()
+          
+    def test_user_cant_see_success_message(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.should_not_be_success_message()
         
     
