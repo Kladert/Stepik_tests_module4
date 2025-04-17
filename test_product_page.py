65ce2bca7d69441e32c8846b54255fb237025557
Kladert <kladert@inbox.ru>
2025-04-17 11:04:10 +0500

    group test

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,8 @@
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
+import time
+import pytest
 
 def test_guest_can_add_product_to_basket(browser):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -16,3 +19,23 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_product_in_basket()
     basket_page.exists_message_basket_is_empty()
+    
+@pytest.mark.login_user 
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        email = str(time.time())+ "@fakemail.org"
+        page.register_new_user(email,"zxc123vbn")
+        page.should_be_authorized_user()
+    
+    def test_user_can_add_product_to_basket(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        time.sleep(5)
+        page.add_product_to_basket()
+        
+    
